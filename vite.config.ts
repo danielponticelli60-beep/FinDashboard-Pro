c9875df10001a97e785f9929501cd8e289e6fd4a
danielponticelli60-beep <danielponticelli60@gmail.com>
2026-08-17 14:56:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig({
+  base: '/FinDashboard-Pro/',
+});
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
